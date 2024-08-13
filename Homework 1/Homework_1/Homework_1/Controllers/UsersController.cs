0389a69c1ff9b8f9592187c4ad3dcfd5d4b6c37a
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-08-13 20:09:16 +0200

    Web Api Homework 1 done

---
+++ Homework 1/Homework_1/Homework_1/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAll()
+        {
+
+            return Ok(StaticDb.Usernames);
+        }
+
+        [HttpGet("index")]
+        public ActionResult<List<string>> GetUserName(int index)
+        {
+            int allUsers = StaticDb.Usernames.Count;
+
+            try
+            {
+                //validations
+                if (index < 0)
+                {
+                    return BadRequest($"This is a negative number, please enter a number from 0 to {allUsers - 1}");
+                }
+
+                if (index >= StaticDb.Usernames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no user on index {index}, please enter a number from 0 to {allUsers - 1}");
+                }
+
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Usernames[index]);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Please contact your administrator");
+            }
+        }
+    }
+}
---
+++ Homework 1/Homework_1/Homework_1/StaticDb.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_1
+{
+    public class StaticDb
+    {
+        public static List<string> Usernames = new List<string>()
+        {
+            "michaelFelps12",
+            "juniperLee003",
+            "benjamin555",
+            "tennyson78",
+            "kevin_Levin",
+            "sean-mcloughlin"
+        };
+    }
+}
